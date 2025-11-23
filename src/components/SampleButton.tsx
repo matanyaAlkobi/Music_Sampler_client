ca866e366ee84ac36636bf8b9eafc726e084e8ad
matanya alkobi <matanor123456@gmail.com>
2025-11-23 12:10:35 +0200

    Refactor SampleButton component to manage active state and update button styling

--- src/components/SampleButton.tsx
+++ src/components/SampleButton.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import * as Tone from "tone";
 type SampleButtonProps = {
   sample: string;
 };
 
 export default function SampleButton({ sample }: SampleButtonProps) {
-  const playSample = async () => {
-    await Tone.start();
-    const player = new Tone.Player(`/piano/${sample}`).toDestination();
-    player.autostart = true;
+  const [active, setActive] = useState<boolean>(false);
+
+  const handleClick = async () => {
+        const newActive = !active;
+    setActive(newActive);
+
+    if (newActive) {
+      await Tone.start();
+      const player = new Tone.Player(`/piano/${sample}`).toDestination();
+      player.autostart = true;
+    }
   };
 
   return (
-    <button className="sample-btn" onClick={playSample}>
+    <button className="sample-btn" onClick={handleClick} style={{background: active ? "red": "#eee"}}>
       {sample.replace(".wav", "")}
     </button>
   );
